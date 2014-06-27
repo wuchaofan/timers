c2c8dfe84380212db5d00eccfd2392d62e5e2850
chaofan <915546302@163.com>
2014-06-27 10:33:48 +0000

    modified:   mulity.py

--- mulity.py
+++ mulity.py
@@ -1,30 +1,40 @@
+from datetime import datetime
 import time,logging
+import setting
 from notify import Notify
 from daemonize import Daemonize
-from Log import LOG
+#from Log import LOG
 def servermain():
-	noo=Notify()
 	try:
 		while True:
-			noo.sendMessage()	
-				
-	# except KeyboardInterrupt, e:
-	# 	LOG(e)
-	except:
-		pass
+			noo=Notify()
+			rest=noo.sendMessage()	
+			noo.close()
+			if not rest:
+				print '=========',datetime.now()
+                		sec=setting.PERIOD-datetime.now().second
+                		print 'sleep....',sec 
+                		time.sleep(sec)
+	except KeyboardInterrupt, e:
+	 	#LOG(e)
+		print e
 	finally:
-		noo.close()
+		pass
+		#noo.close()
 	
 
 	
 if __name__=='__main__':
-	pid="/home/wuchaofan/test_app.pid"
+	pid="test_app.pid"
 	fh = logging.FileHandler("test.log", "w")
 	fh.setLevel(logging.DEBUG)
+	formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+	fh.setFormatter(formatter)
+	logger = logging.getLogger('jobs')
 	logger.addHandler(fh)
 	keep_fds = [fh.stream.fileno()]
 
-	# servermain()
+#	servermain()
 	daemon = Daemonize(app="test_app", pid=pid, action=servermain,keep_fds=keep_fds)
 	daemon.start()
 
