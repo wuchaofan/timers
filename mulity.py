ee1f6894d2d7b7cfa2840270ffc3a3139221ce03
chaofan <915546302@163.com>
2014-07-09 22:04:15 +0800

    modified:   mulity.py

--- mulity.py
+++ mulity.py
@@ -3,21 +3,31 @@ import time,logging
 import setting
 from notify import Notify
 from daemonize import Daemonize
-#from Log import LOG
+
+fh = logging.FileHandler("test.log", "w")
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+fh.setFormatter(formatter)
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s')
+logger = logging.getLogger('jobs')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(fh)
 def servermain():
+	logger = logging.getLogger('jobs')
 	try:
 		while True:
 			noo=Notify()
 			rest=noo.sendMessage()	
 			noo.close()
 			if not rest:
-				print '=========',datetime.now()
-                		sec=setting.PERIOD-datetime.now().second
-                		print 'sleep....',sec 
-                		time.sleep(sec)
+				logger.debug('No data')
+				logger.debug('current time-->%s',datetime.now())
+				sec=setting.PERIOD-datetime.now().second
+				logger.debug('sleep....%ss',sec)
+				time.sleep(sec)
 	except KeyboardInterrupt, e:
 	 	#LOG(e)
-		print e
+		logger.debug(e)
 	finally:
 		pass
 		#noo.close()
@@ -26,15 +36,10 @@ def servermain():
 	
 if __name__=='__main__':
 	pid="timer.pid"
-	fh = logging.FileHandler("test.log", "w")
-	fh.setLevel(logging.DEBUG)
-	formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-	fh.setFormatter(formatter)
-	logger = logging.getLogger('jobs')
-	logger.addHandler(fh)
+	
 	keep_fds = [fh.stream.fileno()]
-
-#	servermain()
-	daemon = Daemonize(app="test_app", pid=pid, action=servermain,keep_fds=keep_fds)
+	logger.debug("Start...")
+	# servermain()
+	daemon = Daemonize(app="jobs", pid=pid, action=servermain,keep_fds=keep_fds)
 	daemon.start()
 
