20a5baa02c2ed5da9a9e4c0ad14fb37be269f496
chaofan <915546302@163.com>
2014-12-01 12:45:52 +0800

    modified:   setting.py

--- setting.py
+++ setting.py
@@ -3,5 +3,8 @@ IOSPEM=os.path.dirname(__file__)+'/ck_test_dev.pem'
 HOST='127.0.0.1'
 USER='root'
 PASSWORD='code'
-DB='db'
+DB='newyiqichang1'
 PERIOD=60
+
+baidupush_apikey=''
+baidupush_secretkey=''
